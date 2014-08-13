485ad698ff08dd8d8cb7e188ac26d7307af30ccd
missett <ryanmissett@ryans-mbp.home>
2014-08-13 18:36:29 +0100

    Pokemon data constructor now uses record syntax

--- Pokemon.hs
+++ Pokemon.hs
@@ -8,4 +8,8 @@ module Pokemon (
 data Id      = Id (Maybe Integer) deriving (Show)
 data Name    = Name String deriving (Show)
 data Species = Species (Maybe Integer) deriving (Show)
-data Pokemon = Pokemon Id Name Species deriving (Show)
+
+data Pokemon = Pokemon { id :: Id 
+					   , name :: Name
+					   , species :: Species 
+					   } deriving (Show)
